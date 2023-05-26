074e9417c4f126f908efe96d23b647df3afa44a6
Joel Jassan <joeljassan@hotmail.com>
2023-05-26 16:05:36 -0300

    Se agregan parametros genericos al modulo tx_rx

--- source/tx_rx_uart.vhd
+++ source/tx_rx_uart.vhd
@@ -9,6 +9,13 @@
 --      Software CuteCom (para abrir el puerto serial)
 --      Hardware CP2102
 --
+-- ---------------------
+-- Consideraciones: el transmisor esta implementado de manera distinta al receptor, por eso es que
+-- hay tantos parametros genericos. 
+-- No entiendo muy bien como esta implementada la "rx_uart_mef". Es el codigo de la ingeniera, 
+-- porque tiene contadores en el medio, en vez de manejar directamente los estados. 
+--
+--
 --
 -- Copyright 2023, Joel Jassan <joeljassan@hotmail.com>
 -- All rights reserved.
@@ -18,7 +25,16 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity rx_tx_uart is
-
+    generic (
+        --Transmisor
+        nbits_tx   : integer := 13;
+        cnt_max_tx : integer := 5209;
+        --Receptor
+        nbits_rx   : integer := 9;
+        cnt_max_rx : integer := 325;
+        --Comun
+        data_lenght : integer := 8
+    )
     port (
         --input ports
         clk    : in std_logic;
@@ -40,6 +56,14 @@ architecture a_rx_tx_uart of rx_tx_uart is
     ----- Signals (i: entrada, o:salida, s:señal intermedia)---------------------------------------
 begin
     ----- Components ------------------------------------------------------------------------------
+
+    transmitter : entity work.tx_uart
+        generic map()
+        port map();
+
+    receiver : entity work.rx_uart
+        generic map()
+        port map();
 
     ----- Codigo ----------------------------------------------------------------------------------
 

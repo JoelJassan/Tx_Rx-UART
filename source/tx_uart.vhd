8ae552ab4025b1260051c2b8d51682937df6afe5
Joel Jassan <joeljassan@hotmail.com>
2023-05-27 21:42:32 -0300

    Se agrega soporte para detectar recepcion de pulso

--- source/tx_uart.vhd
+++ source/tx_uart.vhd
@@ -1,3 +1,15 @@
+-- VHDL file
+--
+-- Autor: Jassan, Joel
+-- Date: (May/2023)
+-- 
+-- Proyect Explanation: Testbench del receptor UART con ROM.
+--
+--
+-- Copyright 2023, Joel Jassan <joeljassan@hotmail.com>
+-- All rights reserved.
+---------------------------------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -11,36 +23,58 @@ entity tx_uart is
 	);
 
 	port (
+		--input ports
 		clk   : in std_logic;
 		reset : in std_logic;
 
-		send : in std_logic; -- se le indica al transmisor que envie "dato"
-		dato : in std_logic_vector (data_lenght - 1 downto 0);
+		send_p : in std_logic; -- se le indica al transmisor que envie "dato"
+		dato_p : in std_logic_vector (data_lenght - 1 downto 0);
 
-		tx : out std_logic
+		--output ports
+		tx_p : out std_logic
 	);
 end entity;
 
 architecture a_tx_uart of tx_uart is
 
+	----- Typedefs --------------------------------------------------------------------------------
+
+	----- Constants -------------------------------------------------------------------------------
+
+	----- Signals (i: entrada, o:salida, s:señal intermedia)---------------------------------------
+
 	-- Reloj
 	signal clk_9600 : std_logic;
 
 	-- Transmisor UART
-	signal salida_tx : std_logic;
+	signal salida_tx  : std_logic;
+	signal send_pulse : std_logic;
 begin
-	--------- COMPONENTES -------------------------------------------
+	----- Components ------------------------------------------------------------------------------
 
 	ut_9600 : entity work.contador_binario
 		generic map(nbits, cnt_max) --(13, 5209)
 		port map(clk, reset, clk_9600, open);
 
 	ut_t_uart : entity work.tx_uart_mef
-		port map(clk_9600, reset, send, dato, salida_tx);
+		port map(clk_9600, reset, send_pulse, dato_p, salida_tx);
 
-	--------- CODIGO ------------------------------------------------
+	----- Codigo ----------------------------------------------------------------------------------
 
+	-- Logica de estado siguiente
+	process (send_p, clk_9600)
+	begin
+		-- Detecta el flanco del pulso. El dato sirve solo si esta en el loop idle-start
+		if (send_p = '1') then
+			send_pulse <= '1';
+			
+		-- Reinicia el estado del pulso.
+		elsif (rising_edge(clk_9600)) then
+			send_pulse <= '0';
+		end if;
+
+	end process;
 	-- Logica de Salida
-	tx <= salida_tx;
+	tx_p <= salida_tx;
 
 end architecture;
